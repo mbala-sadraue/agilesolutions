0a969d47e78b85a32df001bd31756f1ed7ce6cca
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-08-09 22:19:27 +0100

    criar js expecifico

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+        $(function(){
+            $('.center').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 3,
+            autoplay: true,
+            accessibility:false,
+            arrows:false,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+        });
