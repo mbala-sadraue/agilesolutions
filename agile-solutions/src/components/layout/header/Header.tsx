324bab30695a1b7e267dc7e37b1c7b9aca7ee90d
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-10-10 23:58:34 +0100

    adicionar tailwind

---
+++ agile-solutions/src/components/layout/header/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+
+
+    return (
+
+        <>
+            <h2 className="text-red-500">Header</h2>
+        </>
+    );
+
+}
+
+export default Header;
--- agile-solutions/src/main.tsx
+++ agile-solutions/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import Header from './components/layout/header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <BrowserRouter>
-     <App />
-   </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
