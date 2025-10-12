cf5beecd6393ffaa4ac9bd7aa4d05236a42f5309
Fulano de Tal <orlandosadraquiorlandinho9@gmail.com>
2025-10-12 08:19:39 +0100

    terminar menu

--- agile-solutions/src/components/layout/header/Header.tsx
+++ agile-solutions/src/components/layout/header/Header.tsx
@@ -2,6 +2,7 @@ import { BiEnvelope } from "react-icons/bi";
 import { BsClock, BsInstagram, BsLinkedin, BsTwitterX, BsYoutube } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 import { GoLocation } from "react-icons/go";
+import './Header.css';
 
 const Header = () => {
 
@@ -17,81 +18,79 @@ const Header = () => {
                 <div className="bg-primary text-white py-3">
                     <div className="m-auto max-w-7xl">
                         <div className=" flex flex-1  md:justify-between justify-center ">
-                            
-                                <ul className="md:flex flex-1 space-x-3 text-sm hidden">
-                                    <li className=" flex space-x-2 items-center">
-                                        <i className=""><BsClock className="text-secondary text-secondary text-[1rem]" /> </i>
-                                        <span>Secunda - Sexta:
-                                            8:00 - 18:00 / Sábado 8:00 - 14:00</span>
-                                    </li>
-                                    <li className="flex space-x-2 items-center">
-                                        <i className=""><BiEnvelope className="text-secondary text-[1.1rem]"/> </i>
-                                        <a href="mailto:geral@agilesolutions.ao">geral@agilesolutions.ao</a>
-                                    </li>
 
-                                    <li className="flex space-x-2 items-center">
-                                        <i className=""><GoLocation className="text-secondary text-[1.1rem]"/> </i>
-                                        <span>Jardim de Rosa, Luanda - Angola</span>
+                            <ul className="md:flex flex-1 space-x-3 text-sm hidden">
+                                <li className=" flex space-x-2 items-center">
+                                    <i className=""><BsClock className="text-secondary text-secondary text-[1rem]" /> </i>
+                                    <span>Secunda - Sexta:
+                                        8:00 - 18:00 / Sábado 8:00 - 14:00</span>
+                                </li>
+                                <li className="flex space-x-2 items-center">
+                                    <i className=""><BiEnvelope className="text-secondary text-[1.1rem]" /> </i>
+                                    <a href="mailto:geral@agilesolutions.ao">geral@agilesolutions.ao</a>
+                                </li>
 
-                                    </li>
+                                <li className="flex space-x-2 items-center">
+                                    <i className=""><GoLocation className="text-secondary text-[1.1rem]" /> </i>
+                                    <span>Jardim de Rosa, Luanda - Angola</span>
+
+                                </li>
 
 
-                                </ul>
-                          
+                            </ul>
+
                             <div className="flex items-center space-x-4 ">
                                 <span className="text-sm font-normal">Siga-nos em:</span>
-                                <a href="https://www.facebook.com/" rel="noopener noreferrer"   target="_blank"><i className="hover:text-amber-400"><FaFacebookF /> </i></a>
-                                <a href="https://www.twitter.com/"  rel="noopener noreferrer"  target="_blank"><i className="hover:text-amber-400"><BsTwitterX /> </i></a>
-                                <a href="https://www.linkedin.com/"  rel="noopener noreferrer"  target="_blank"> <i className="hover:text-amber-400"><BsLinkedin /> </i></a>
-                                <a href="https://www.instagram.com/"  rel="noopener noreferrer"  target="_blank"><i className="hover:text-amber-400"><BsInstagram /> </i></a>
-                                <a href="https://www.youtube.com/" rel="noopener noreferrer"   target="_blank"><i className="hover:text-amber-400"><BsYoutube /> </i></a>
+                                <a href="https://www.facebook.com/" rel="noopener noreferrer" target="_blank"><i className="hover:text-amber-400"><FaFacebookF /> </i></a>
+                                <a href="https://www.twitter.com/" rel="noopener noreferrer" target="_blank"><i className="hover:text-amber-400"><BsTwitterX /> </i></a>
+                                <a href="https://www.linkedin.com/" rel="noopener noreferrer" target="_blank"> <i className="hover:text-amber-400"><BsLinkedin /> </i></a>
+                                <a href="https://www.instagram.com/" rel="noopener noreferrer" target="_blank"><i className="hover:text-amber-400"><BsInstagram /> </i></a>
+                                <a href="https://www.youtube.com/" rel="noopener noreferrer" target="_blank"><i className="hover:text-amber-400"><BsYoutube /> </i></a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="header-main">
-                    <nav className="navbar navbar-expand-lg">
-                        <div className="container-fluid">
-                            <a className="navbar-brand" href="#">Navbar</a>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+
+                <nav className="">
+                    <div className="flex space-x-3 max-w-7xl m-auto items-center">
+                        <a className="" href="#">Navbar</a>
+                        {/* <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas"
                                 data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
                                 aria-label="Toggle navigation">
                                 <i className="fa fa-bars"></i>
-                                {/* <span className="navbar-toggler-icon"></span> */}
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                    <li className="nav-item">
-                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="about.html">Sobre Nos</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Serviços</a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#">Contacto</a>
-                                    </li>
+                             
+                            </button> */}
+                        <div className="flex flex-1 items-center py-5" id="">
+                            <ul className="flex flex-1 space-x-2">
+                                <li className="nav-item">
+                                    <a className="text-sm  hover:bg-amber-100/50  rounded-2xl transition duration-300" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="text-sm hover:bg-amber-100/50  rounded-2xl transition duration-300" href="">Sobre Nos</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="text-sm hover:bg-amber-100/50  rounded-2xl transition duration-300" href="#">Serviços</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="text-sm hover:bg-amber-100/50  rounded-2xl transition duration-300" href="#">Contacto</a>
+                                </li>
 
-                                </ul>
-                                <div className="col-auto d-none d-xl-block">
-                                    <div className="header-button">
-                                        <a className="sideMenuToggler icon-btn" href="#">
-                                            {/* <img src="assets/images/icons/menu.svg" alt="" srcset=""> */}
-                                        </a>
-                                        <a className="btn th-btn " href="tel:+244935378674">
-                                            <i className="fa-light fa-phone"></i> ligue para nos ajudar: +244 935 378 674
+                            </ul>
+                            <div className="">
+                                <div className="flex space-x-3">
+                                    <a className="sideMenuToggler icon-btn" href="#">
+                                        {/* <img src="assets/images/icons/menu.svg" alt="" srcset=""> */}
+                                    </a>
+                                    <a className="btn-contact text-sm" href="tel:+244935378674">
+                                        <i className="fa-light fa-phone"></i> ligue para nós: +244 935 378 674
 
-                                        </a>
-                                    </div>
+                                    </a>
                                 </div>
                             </div>
                         </div>
-                        <div className="logo-bg bg-mask"></div>
-                    </nav>
-                </div>
+                    </div>
+                </nav>
                 {/* <div className="offcanvas offcanvas-end" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div className="offcanvas-header">
                         <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
